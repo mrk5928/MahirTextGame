114a8073858ef05b398d8cb4043f39656a920791
Mahir Khan <mrk5928@psu.edu>
2023-01-25 21:46:21 -0500

    New updates

--- Text Adventure Game/src/Main.java
+++ Text Adventure Game/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        Item first_item = new Item("Key", "Used to open Vaults", 5);
+        Item second_item = new Item("Mechanical Parts", "Resource obtained by from harvesting cars", 10);
+        Item third_item = new Item("MotorBoat", "Boat which is equipped with missiles.", 25);
+        Item fourth_item = new Item("The Mandalorian Jetpack", "Allows the player to hover or glide in the air for a short time. Unlimited Fuel", 30);
 
     }
 }
---
+++ Text Adventure Game/src/Pillow.java
@@ -0,0 +1,6 @@
+public class Pillow extends Weapon{
+
+    public Pillow(String name, String description, int value, int i) {
+        super("Pillow", "An extremely soft object that players can use while resting", 7, 14);
+    }
+}
